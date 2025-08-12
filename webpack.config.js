9cfa96ccba33ab75f97c736b001dac81e653c344
Yarden Porat <yardenporat@gmail.com>
2025-08-12 08:58:59 +0300

    1.0.9 backwards compat css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ZipPlugin = require('zip-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = process.env.NODE_ENV === 'development';
@@ -13,6 +15,8 @@ module.exports = {
         content: './src/content.ts',
         popup: './src/popup.ts',
         background: './src/background.ts',
+        style: './static/style.css',
+        'toggle-button': './static/toggle-button.css',
     },
     output: {
         filename: '[name].js',
@@ -25,18 +29,26 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },
             isDev && {
                 test: /\.js$/,
                 enforce: 'pre',
                 loader: 'source-map-loader',
             },
-        ],
+        ].filter(Boolean),
     },
     resolve: {
         extensions: ['.ts', '.js'],
     },
 
     plugins: [
+        new RemoveEmptyScriptsPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
         new CopyWebpackPlugin({
             patterns: [{ from: 'static' }],
         }),
